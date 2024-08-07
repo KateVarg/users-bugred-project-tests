d1e7ccb3f21ef511e8368718b5f30f1201955607
KateVarg <katye9470@gmail.com>
2024-08-07 20:47:42 +0200

    fix allure annotations

--- tests/add_task_page_test.py
+++ tests/add_task_page_test.py
@@ -1,14 +1,21 @@
 from diplom_users_tests.data.profile_task import Task
 from diplom_users_tests.pages.profile_tasks_page import open_page_add_task, add_task
 from diplom_users_tests.pages.login_page import user_login_api
+import allure
 
 
+@allure.feature("Добавление новой задачи")
+@allure.story("Переход на страницу добавления новой задачи")
+@allure.title("Проверка перехода на страницу добавления новой задачи")
 def test_open_page_add_task(generated_user_data):
     user_login_api.login_api(generated_user_data).check_code().get_cookie()
     user_login_api.add_cookie()
     open_page_add_task.open().click_button_add_task().check_open_page_add_task()
 
 
+@allure.feature("Добавление новой задачи")
+@allure.story("Добавление новой задачи")
+@allure.title("Проверка добавления новой задачи")
 def test_add_task(generated_user_data):
     user_login_api.login_api(generated_user_data).check_code().get_cookie()
     user_login_api.add_cookie()
--- tests/users_page_test.py
+++ tests/users_page_test.py
@@ -1,7 +1,11 @@
 from diplom_users_tests.data.data_search import SearchUser
 from diplom_users_tests.pages.users_page import user_search, open_profile_page
+import allure
 
 
+@allure.feature("Список пользователей")
+@allure.story("Поиск пользователя")
+@allure.title("Поиск существующего пользователя")
 def test_search_user():
     user = SearchUser(
         '2018',
@@ -15,6 +19,9 @@ def test_search_user():
     user_search.open().register(user).check_search_result(user)
 
 
+@allure.feature("Список пользователей")
+@allure.story("Профиль пользователя")
+@allure.title("Переход на страницу с профилем пользователя")
 def test_open_profile_page():
     open_profile_page.open().click_show_button().check_profile()
 
